07ac87140fc82d90e39752bf409dbdec7807ba82
Elijah Sawyers <elijahsawyers@gmail.com>
2019-07-22 07:45:42 -0500

    File renamed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     ],
     entry_points={
         'console_scripts': [
-            'autostack=autostack.__main__:main'
+            'autostack=autostack.main:main'
         ]
     },
     install_requires=[
