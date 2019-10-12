7fbe92bb282af05a2eabdc53412a3a9f5fd6169f
Elijah Sawyers <elijahsawyers@gmail.com>
2019-10-12 15:37:35 -0500

    Removed features package

--- autostack/features/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Authors: Elijah Sawyers, Benjamin Sanders
-Emails: elijahsawyers@gmail.com, ben.sanders97@gmail.com
-Date: 07/29/2019
-Overview: Contains various extra features and options for autostack
-'''
