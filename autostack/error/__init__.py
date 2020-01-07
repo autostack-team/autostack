dbc9faad5a653a62950fdcbe6202d0bbf9bd5cee
Elijah Sawyers <elijahsawyers@gmail.com>
2020-01-06 21:51:16 -0600

    Working on multiple language support.

--- autostack/error/__init__.py
+++ autostack/error/__init__.py
@@ -2,7 +2,7 @@
 Authors: Elijah Sawyers, Benjamin Sanders
 Emails: elijahsawyers@gmail.com, ben.sanders97@gmail.com
 Date: 10/09/2019
-Overview: Ability to handle exceptions.
+Overview: Ability to handle errors.
 '''
 
 from __future__ import (
@@ -31,7 +31,8 @@ def listen_for_errors(pipe, config):
     output is parsed for errors.
 
     Parameter {file} pipe: the pipe to read output from.
-    Parameter {dictionary} config: configuration object.
+    Parameter {dictionary} config: configuration object passed in from
+    the "display" cli command.
     '''
 
     print_logo()
@@ -44,38 +45,85 @@ def listen_for_errors(pipe, config):
         if output == '':
             break
         
-        parse_output_for_error(pipe, output, config)
+        detect_and_handle_error(pipe, output, config)
 
 
-def parse_output_for_error(pipe, output, config):
+def detect_and_handle_error(pipe, output, config):
     '''
+    Loops over the configured languages and parses for an error for
+    each language. If an error is detected for a language, it is handled.
+
+    Parameter {string} language: the language to parse for an outputted error.
+    Parameter {string} output: the current line of output captured with the
+    "capture" cli command.
+    Parameter {dictionary} config: configuration object passed in from
+    the "display" cli command.
     '''
+
+    error_handled = False
 
     for language in get_configured_languages(config):
-        error_lib = importlib.import_module('autostack.error.{}'.format(language.lower()))
-        error = error_lib.parse_output_for_error(output, pipe)
+        error = get_error(language, output, pipe)
 
         if error:
-            handle_exception(error)
-            print_listening_for_errors()
+            handle_error(error)
+            error_handled = True
+
+    if error_handled:
+        print_listening_for_errors()
+
+
+def get_error(language, output, pipe):
+    '''
+    With the given configured language, import the appropriate parser
+    to retrieve an (potential) error in the terminal. This function will
+    return None if there are no errors detected with the given output and
+    the configured language.
+
+    Parameter {string} language: the language to parse for an outputted error.
+    Parameter {string} output: the current line of output captured with the
+    "capture" cli command.
+    Parameter {file} pipe: the pipe being used to capture terminal output.
+    Returns {string|None}: the error, or None, if there isn't one detected.
+    '''
+
+    try:
+        error_lib = importlib.import_module(
+            'autostack.error.{}'.format(language.lower())
+        )
+
+        return error_lib.parse_output_for_error(output, pipe)
+    except ImportError:
+        print('The language {} is not yet supported.'.format(language))
 
 
 def get_configured_languages(config):
     '''
+    Gets the configured languages to display errors for. The order
+    of retrieval is as follows:
+
+    1. "diplay" command configuration.
+    2. Local .autostack.json configuration.
+    3. Global .autostack.json configuration.
+
+    Returns {list}: the list of configured languages.
     '''
 
+    # Configs set by the "display" cli command.
     try:
         return config.languages
     except:
+        # Local configurations.
         if get_config(False, 'languages'):
             return get_config(False, 'languages')
+        # Global configurations.
         elif get_config(True, 'languages'):
             return get_config(True, 'languages')
-    
+
     return []
 
 
-def handle_exception(query):
+def handle_error(query):
     '''
     When passed a query, this function loops over each accepted
     Stack Overflow post, and displays them, until the user inputs
@@ -93,7 +141,7 @@ def handle_exception(query):
 
         # Custom query.
         if user_input not in (True, False):
-            handle_exception(user_input)
+            handle_error(user_input)
             return
 
         # Error solved, break out of the loop.
