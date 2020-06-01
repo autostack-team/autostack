33286c9aa42948ccd8bd0595d74ac3bec2db5d5d
Elijah Sawyers <elijahsawyers@gmail.com>
2020-05-31 23:11:38 -0500

    Working on so_web_scraper tests

---
+++ autostack/so_web_scraper/__tests__/test_so_web_scraper_print.py
@@ -0,0 +1,19 @@
+'''
+Authors: Elijah Sawyers
+Emails: elijahsawyers@gmail.com
+Date: 05/31/2020
+Overview: Tests for the so_web_scraper print module.
+'''
+
+
+def test_placeholder():
+    '''
+    Ensures...
+    '''
+
+    # 1. Given.
+
+    # 2. When.
+
+    # 3. Then.
+    assert True
---
+++ autostack/so_web_scraper/__tests__/test_so_web_scraper_scrape.py
@@ -0,0 +1,19 @@
+'''
+Authors: Elijah Sawyers
+Emails: elijahsawyers@gmail.com
+Date: 05/31/2020
+Overview: Tests for the so_web_scraper scrape module.
+'''
+
+
+def test_placeholder():
+    '''
+    Ensures...
+    '''
+
+    # 1. Given.
+
+    # 2. When.
+
+    # 3. Then.
+    assert True
